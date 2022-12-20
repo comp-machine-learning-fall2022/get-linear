151514f87d9fdaaebe046a71ddfafd11534f2282
anishaj149 <anisha.j149@gmail.com>
2022-12-20 13:20:39 -0500

    adding project parts

---
+++ Spectral-Clustering/README.md
@@ -0,0 +1,13 @@
+# Let's Get Linear: Spectral Clustering
+
+Using spectral clustering instead of a plain old k-means clustering algorithm can bring about some interesting patterns in data. Spectral clustering a form of dimension reduction with a bunch of linear algebra concepts behind it, so let's explore it! 
+
+## Important Files
+1. `spectral-clustering-notebook.ipynb` - interactive file for exploring spectral clustering
+2. `spec_clust.py` - python file that contains spectral clustering functions and code for running methods locally
+3. `spec_clust_data.csv` - data file for toy example
+4. `test_spec_clust` - testing file for python file
+
+## Sources
+- https://www.researchgate.net/figure/An-example-of-the-Laplacian-matrix-of-a-simple-network-n-4_fig1_305653264
+- https://www.nature.com/articles/s41598-017-01010-0
---
+++ Spectral-Clustering/test_spec_clust.py
@@ -0,0 +1,102 @@
+import pytest
+import pandas as pd
+import numpy as np
+import spec_clust as sc
+
+my_data = np.loadtxt("spec_clust_data.csv", delimiter = ",")
+
+#### Tests for Adjacency Matrix ####
+
+def test_make_adj_size():
+	expected = (1000,1000)
+	assert sc.make_adj(my_data).shape == expected
+
+def test_make_adj_diag():
+	# Test for empty diagonal
+	expected = 0
+	out = sc.make_adj(my_data)
+	assert np.sum(np.diag(out)) == expected
+
+def test_make_adj_values():
+	# Test that the matrix is binary
+	expected = [0, 1]
+	out = sc.make_adj(my_data)
+	assert list(np.unique(out)) == expected
+
+#### Tests for Laplacian ####
+
+def test_my_laplacian_size():
+	expected = (1000,1000)
+	AM = sc.make_adj(my_data)
+	assert sc.my_laplacian(AM).shape == expected
+
+def test_my_laplacian_diag():
+	# Test that the degree matrix diagonal is 
+	#    non-negative
+	expected = 1000
+	AM = sc.make_adj(my_data)
+	out = sc.my_laplacian(AM)
+	assert np.sum(np.diag(out)>=0) == expected
+
+def test_my_laplacian_else():
+	expected = 1000*1000
+	AM = sc.make_adj(my_data)
+	out = sc.my_laplacian(AM)
+	#Consider the top triangular half of the matrix
+	upt_out = np.triu(out,1)
+	#Consider the lower triangular half of the matrix
+	downt_out = np.tril(out,-1)
+	test_mat = upt_out + downt_out
+	assert np.sum(test_mat<=0) == expected
+
+#### Test for Kmeans ####
+
+def test_full_kmeans():
+	assert isinstance(sc.full_kmeans(my_data,6), tuple)
+
+def test_full_kmeans_shape():
+	expected = 2
+	assert len(sc.full_kmeans(my_data,6)) == expected
+
+def test_full_kmeans_labels():
+	expected = 5
+	label_max = np.max(sc.full_kmeans(my_data,6)[0])
+	assert label_max == expected
+
+def test_full_kmeans_center_num():
+	expected = (6,2)
+	centers_shape = sc.full_kmeans(my_data, 6)[1].shape
+	assert centers_shape == expected
+
+#### Spectral clustering tests ####
+
+def test_spect_clustering():
+	AM = sc.make_adj(my_data)
+	L = sc.my_laplacian(AM)
+	out = sc.spect_clustering(L,7)
+	assert isinstance(out, tuple)
+
+def test_spect_clustering_shape():
+	expected = 2
+	AM = sc.make_adj(my_data)
+	L = sc.my_laplacian(AM)
+	out = sc.spect_clustering(L,7)
+	assert len(out) == expected
+
+def test_spect_clustering_labels():
+	expected = 6
+	AM = sc.make_adj(my_data)
+	L = sc.my_laplacian(AM)
+	out = sc.spect_clustering(L,7)
+	label_max = np.max(out[0])
+	assert label_max == expected
+
+def test_spect_clustering_center_num():
+	expected = (7,7)
+	AM = sc.make_adj(my_data)
+	L = sc.my_laplacian(AM)
+	out = sc.spect_clustering(L,7)
+	centers_shape = out[1].shape
+	assert centers_shape == expected
+
+
